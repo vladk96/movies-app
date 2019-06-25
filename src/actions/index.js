87c7448bdca95b0eca9408cb365cede909047e37
vladk96 <vladk961@gmail.com>
2019-06-25 23:47:16 +0300

    add home actions

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import { fetchMovies } from '../api/api';
+
+export const GET_ALL_MOVIES = 'GET_ALL_MOVIES';
+export const GET_MOVIE = 'GET_MOVIE';
+export const LOAD_MORE_MOVIES = 'LOAD_MORE_MOVIES';
+export const CLEAR_MOVIES = 'CLEAR_MOVIES';
+export const SHOW_LOADER = 'SHOW_LOADER';
+
+export const getAllMovies = () => {
+  const request = fetchMovies();
+
+  return {
+    type: GET_ALL_MOVIES,
+    payload: request,
+  };
+};
+
+export const showLoader = () => ({
+  type: SHOW_LOADER,
+  payload: null,
+});
+
+export const clearMovies = () => ({
+  type: CLEAR_MOVIES,
+  payload: null,
+});

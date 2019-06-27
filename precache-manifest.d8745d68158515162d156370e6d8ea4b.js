e588b98294ea1dbe6985e310ae6a472aff3bf65b
vladk96 <vladk961@gmail.com>
2019-06-27 19:46:25 +0300

    Updates

---
+++ precache-manifest.d8745d68158515162d156370e6d8ea4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f354de97a45145bd511cc8acbf8d467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e535d4a3d6ebc81e6e53",
+    "url": "/static/css/2.88cc0142.chunk.css"
+  },
+  {
+    "revision": "8c9f81566f3e40847cdc",
+    "url": "/static/css/main.251523cf.chunk.css"
+  },
+  {
+    "revision": "e535d4a3d6ebc81e6e53",
+    "url": "/static/js/2.8f0ae348.chunk.js"
+  },
+  {
+    "revision": "8c9f81566f3e40847cdc",
+    "url": "/static/js/main.7e3ed70e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/static/media/themoviedb-logo.02a9430b.svg"
+  }
+]);
